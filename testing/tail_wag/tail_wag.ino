00def8fbc1f44d0923b5009293989c4661336644
Verna Koskinen <darth.kipsu@gmail.com>
2017-01-26 10:01:01 +0200

    add example to comments

--- testing/tail_wag/tail_wag.ino
+++ testing/tail_wag/tail_wag.ino
@@ -16,6 +16,7 @@ void wagTailSlow(int speed, int degrees) {
   // TODO:
   // Use millis() instead of delay so the board does not wait until
   // done and can maintain cencor activity.
+  // example: https://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
   
   for (int i = 0; i <= degrees / 2; i += 5) {
     tail.write(90 - i);
