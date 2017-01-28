3500f1105684a3343b741857eb9f5505519cacd6
Verna Koskinen <darth.kipsu@gmail.com>
2017-01-28 11:48:05 +0200

    refactor lifting and withdrawing legs

--- body/body.ino
+++ body/body.ino
@@ -14,7 +14,7 @@ void setup() {
 void loop() {
   stand();
   delay(2000);
-  raise_left_front_leg();
+  step_forward(2);
 }
 
 void stand() {
@@ -24,21 +24,79 @@ void stand() {
   front_leg_left.write(90);
 }
 
-void raise_left_front_leg() {
-  for (int i = 0; i < 30; i++) {
-    front_leg_left.write(90 - i);
-    if (i % 5 == 0) {
-      back_leg_left.write(90 - i / 5);
+void step_forward(int steps) {
+  for (int step = 0; step < steps; step++) {
+    for (int i = 0; i < 30; i++) {
+      step_with_left_leg(i);
+      delay(10);
     }
-    delay(10);
-  }
-  delay(500);
-  for (int i = 30; i > 0; i--) {
-    front_leg_left.write(90 - i);
-    if (i % 5 == 0) {
-      back_leg_left.write(90 - i / 5);
+    delay(100);
+    for (int i = 30; i > 0; i--) {
+      step_with_left_leg(i);
+      delay(10);
     }
-    delay(10);
+    stand();
+    for (int i = 0; i < 30; i++) {
+      step_with_right_leg(i);
+      delay(10);
+    }
+    delay(100);
+    for (int i = 30; i > 0; i--) {
+      step_with_right_leg(i);
+      delay(10);
+    }
+    stand();
+    delay(50);
   }
+}
+
+/** Moves left front and right back leg forwards to given degrees and
+/*  withdraws the other two legs to the same degree. **/
+void step_with_left_leg(int degree) {
+  lift_front_leg_left(degree);
+  lift_back_leg_right(degree);
+  withdraw_front_leg_right(degree);
+  withdraw_back_leg_left(degree);
+}
+
+/** Moves right front and left back leg forwards to given degrees and
+/*  withdraws the other two legs to the same degree. **/
+void step_with_right_leg(int degree) {
+  lift_front_leg_right(degree);
+  lift_back_leg_left(degree);
+  withdraw_front_leg_left(degree);
+  withdraw_back_leg_right(degree);
+}
+
+void lift_front_leg_right(int degree) {
+  front_leg_right.write(90 + degree);
+}
+
+void lift_front_leg_left(int degree) {
+  front_leg_left.write(90 - degree);
+}
+
+void lift_back_leg_right(int degree) {
+  back_leg_right.write(90 + degree);
+}
+
+void lift_back_leg_left(int degree) {
+  back_leg_left.write(90 - degree);
+}
+
+void withdraw_front_leg_right(int degree) {
+  front_leg_right.write(90 - degree);
+}
+
+void withdraw_front_leg_left(int degree) {
+  front_leg_left.write(90 + degree);
+}
+
+void withdraw_back_leg_right(int degree) {
+  back_leg_right.write(90 - degree);
+}
+
+void withdraw_back_leg_left(int degree) {
+  back_leg_left.write(90 + degree);
 }
 
